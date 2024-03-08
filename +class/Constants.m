5b4a1c0ec969630833b4a24a45339a6b550cfb27
Eric <75496918+EricMagalhaesDelgado@users.noreply.github.com>
2024-03-08 09:34:46 -0500

    Compilada v. 1.59
    Corrigido bug no módulo "Análise de sinais" e implementada abertura do módulo "RFDataHub" a partir do "Análise de sinais", simulando enlace.

--- +class/Constants.m
+++ +class/Constants.m
@@ -4,7 +4,7 @@ classdef (Abstract) Constants
         %-----------------------------------------------------------------%
         appName       = 'appAnaliseV2'
         appRelease    = 'R2024a'
-        appVersion    = '1.58'
+        appVersion    = '1.59'
 
         windowSize    = [1244, 660]
         windowMinSize = [ 880, 660]
