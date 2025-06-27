f9857c95db53881816f00d104781375f042d6c5d
Eric <75496918+EricMagalhaesDelgado@users.noreply.github.com>
2025-06-27 09:09:42 -0300

    Ajustado template Grandes Eventos

--- src/+class/Constants.m
+++ src/+class/Constants.m
@@ -4,7 +4,7 @@ classdef (Abstract) Constants
         %-----------------------------------------------------------------%
         appName       = 'appAnalise'
         appRelease    = 'R2024a'
-        appVersion    = '1.87.10'
+        appVersion    = '1.87.11'
 
         windowSize    = [1244, 660]
         windowMinSize = [ 950, 660]
